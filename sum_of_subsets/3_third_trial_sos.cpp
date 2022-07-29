b997c225ea9e539acc95a42e1bdf6c4c57c848b7
piru72 <parvezdirom2000@gmail.com>
2022-07-29 15:35:23 +0600

    explained the code

--- sum_of_subsets/3_third_trial_sos.cpp
+++ sum_of_subsets/3_third_trial_sos.cpp
@@ -2,24 +2,23 @@
 using namespace std;
 
 #define mp make_pair
-vector<pair<int, int>> user_input;
 
-int sum_of_all = 0;
-int sum_wanted = 0;
-int sum_of_negative = 0;
-int maximum_possible_sum = 0;
-int minimum_possible_sum = 0;
+int sum_of_all = 0;           // total sum of all the elements
+int sum_wanted = 0;           // the sum wanted form the subsets
+int maximum_possible_sum = 0; // maximum possible sum of the set
+int minimum_possible_sum = 0; // minimum possible sum of the set
 
-int negative_count = 0;
-int positive_count = 0;
+bool negative_exits = false; // checking for negative element
+bool positive_exits = false; // checking for positive element
 
 typedef vector<int> v_int;
 typedef pair<int, v_int> vi;
 typedef pair<int, vi> vii;
 typedef pair<int, vii> viii;
-vector<viii> jumbo;
-
+// storing  sum ,  level , sum in hand , route
 stack<viii> s;
+// storing value and initial given indices
+vector<pair<int, int>> user_input;
 
 void take_user_input(int n)
 {
@@ -30,42 +29,40 @@ void take_user_input(int n)
         cin >> x;
         if (x < 0)
         {
-            sum_of_negative += x;
-            negative_count += 1;
+            minimum_possible_sum += x; // finding sum of all negative number
+            negative_exits = true;
         }
         else
         {
-            positive_count += 1;
-            maximum_possible_sum += x;
+            maximum_possible_sum += x; // finding sum of all positive number
+            positive_exits = true;
         }
-        sum_of_all += x;
+        sum_of_all += x; // sum of all elements
 
-        user_input.push_back(make_pair(x, i));
+        user_input.push_back(make_pair(x, i)); // storing value and given order
     }
-    // cout << "Enter the sum you want " << endl;
+    // cout << "Enter the sum you want " << endl; //sum wanted from  the subset
     cin >> sum_wanted;
 }
 
 void set_min_max(vector<pair<int, int>> v)
 {
-    if (negative_count == 0)
+    if (!negative_exits) // if there is no negative element
     {
         sort(v.begin(), v.end());
-        minimum_possible_sum = v[0].first;
+        minimum_possible_sum = v[0].first; // the lowest positive number is the minimum possible sum
     }
 
-    else
-        minimum_possible_sum = sum_of_negative;
-    if (positive_count == 0)
+    if (!positive_exits) // if there is no positive element
     {
         sort(v.begin(), v.end(), greater<>());
-        maximum_possible_sum = v[0].first;
+        maximum_possible_sum = v[0].first; // the highest negative number is the maximum possible sum
     }
 
     cout << "Maximum possible sum is  " << maximum_possible_sum << endl;
     cout << "Minimum possible sum is  " << minimum_possible_sum << endl;
 }
-void show_vector(vector<int> v)
+void show_vector(vector<int> v) // printing the vector
 {
     for (int i = 0; i < v.size(); i++)
     {
@@ -74,82 +71,69 @@ void show_vector(vector<int> v)
     cout << endl;
 }
 
-void show_user_input(vector<pair<int, int>> v)
-{
-    for (int i = 0; i < v.size(); i++)
-    {
-        cout << v[i].first << " " << v[i].second << endl;
-    }
-}
-
 void push_into_stack()
 {
-    v_int route(user_input.size(), 0);
-    jumbo.push_back(mp(0, mp(1, mp(sum_of_all, route))));
-    s.push(jumbo[0]);
+    v_int route(user_input.size(), 0);           // initializing the route
+    s.push(mp(0, mp(0, mp(sum_of_all, route)))); // initializing the stack as level = 0
     int curr_key;
     int curr_level;
     int curr_sum;
     v_int curr_route;
-    int val_pos;
-    int i = 1;
+    int value_indice;
+    bool first_push = true;
     while (!s.empty())
     {
 
-        if ((s.top().first < sum_wanted && s.top().second.second.first != 0) || i == 1)
+        if ((s.top().first < sum_wanted && s.top().second.second.first != 0) || first_push)
         {
-            curr_key = s.top().first;
-            curr_level = s.top().second.first;
-            curr_sum = s.top().second.second.first;
-            curr_route = s.top().second.second.second;
-            val_pos = user_input[curr_level - 1].second;
+            curr_key = s.top().first;                     //  retrieving the sum of subsets
+            curr_level = s.top().second.first;            //  retrieving  level
+            curr_sum = s.top().second.second.first;       //  retrieving  sum in hand
+            curr_route = s.top().second.second.second;    // retrieving the route
+            value_indice = user_input[curr_level].second; // index number of value to be added
 
-            s.pop();
+            s.pop();                                  // popping from the stack
+            curr_sum -= user_input[curr_level].first; // deleting the set element value
 
-            curr_route[val_pos] = 0;
-            curr_sum -= user_input[curr_level - 1].first;
-            curr_level += 1;
+            s.push(mp(curr_key, mp(curr_level + 1, mp(curr_sum, curr_route)))); // pushing right child
 
-            s.push(mp(curr_key, mp(curr_level, mp(curr_sum, curr_route))));
+            curr_key += user_input[curr_level].first; // addign the set element value
+            curr_route[value_indice] = 1;             // setting route[i] = 1 as left child
 
-            curr_key += user_input[curr_level - 2].first;
-            curr_route[val_pos] = 1;
-
-            s.push(mp(curr_key, mp(curr_level, mp(curr_sum, curr_route))));
-            i++;
+            s.push(mp(curr_key, mp(curr_level + 1, mp(curr_sum, curr_route)))); // pushing left child
+            first_push = false;                                                 // first time pushed
         }
-        else if (s.top().first == sum_wanted)
+        else if (s.top().first == sum_wanted) // if the sum is found
         {
             cout << "The found route for the wanted sum is" << endl;
-            show_vector(s.top().second.second.second);
+            show_vector(s.top().second.second.second); // printing the route
             s.pop();
         }
         else if (s.top().first > sum_wanted || s.top().second.second.first == 0)
         {
-            s.pop();
+            s.pop(); // if it is greater than popping it
         }
-     
     }
 }
 
 int main()
 {
     int n;
-
+    // cout << "Enter total number of elements" <<endl;
     cin >> n;
 
-    take_user_input(n);
+    take_user_input(n); // take user input
 
     set_min_max(user_input);
 
-    if (maximum_possible_sum < sum_wanted || sum_wanted < minimum_possible_sum)
+    if (maximum_possible_sum < sum_wanted || sum_wanted < minimum_possible_sum) // checking the range
     {
         cout << "Wanted sum is out of range\nNot possible to find the sum " << sum_wanted << endl;
     }
 
     else
     {
-        if (sum_wanted < 0)
+        if (sum_wanted < 0) // if the sum wanted is negative flipping all the signs
         {
             sum_wanted *= -1;
             sum_of_all *= -1;
@@ -158,7 +142,7 @@ int main()
                 user_input[i].first *= -1;
             }
         }
-        sort(user_input.begin(), user_input.end());
-        push_into_stack();
+        sort(user_input.begin(), user_input.end()); // sorting the values in ascending order
+        push_into_stack();                          // starting the process
     }
 }
